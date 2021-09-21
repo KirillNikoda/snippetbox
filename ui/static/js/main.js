20d33d37ff9c62d2d9d05a0c91b3fdd0a067c0c0
KirillNikoda <nikodakirill386@gmail.com>
2021-09-21 21:24:06 +0300

    deleted ui

--- ui/static/js/main.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll("nav a");
-for (let i = 0; i < navLinks.length; i++) {
-	const link = navLinks[i];
-	if (link.getAttribute('href') == window.location.pathname) {
-		link.classList.add("live");
-		break;
-	}
-}
